54873ed4b21561bd108550c60b3ae6ca0cc22113
Seymour Makarov <seymour.makarov@gmail.com>
2016-04-26 01:39:37 +0300

    Recursive function for getting elements of simple type

--- main.py
+++ main.py
@@ -25,19 +25,55 @@ for el in elements:
     referenced_node = el.find("[@ref]")
     if referenced_node != None:
         element_name = referenced_node.attrib["ref"]
-        path = ".//element[@name='{}']".format(element_name)
+        path = ".//xs:element[@name='{}']".format(element_name)
         node = tree.find(path, ns)
         if node != None:
-            print node.attrib['name']
-        fin_config_nodes.append(node)
+            fin_config_nodes.append(node)
     else:
-        direct_node = el.find("[@name]")
-        if direct_node != None:
-            print direct_node.attrib['name']
+        fin_config_nodes.append(el)
     # if found an appropriate node add it to node list
 
 
+def get_attributes(attrs):
+    for attr in attrs:
+        print attr.attrib['name']
+
+def get_children(node):
+    # if not empty, argument path should end with '/'
+    # otherwise no magic is guaranteed =)
+    temp_config = dict()
+    ct_path = ".//xs:complexType"
+    els_path = ".//xs:element"
+    attr_path = ".//xs:attribute"
+    complex_type = node.find(ct_path, ns)
+
+    if complex_type is not None:
+        temp_config[node.attrib['name']] = dict()
+        attrs = complex_type.findall(attr_path, ns)
+        if attrs is not None:
+            temp_config[node.attrib['name']]['attribs'] = []
+            for attr in attrs:
+                temp_config[node.attrib['name']]['attribs'].append(attr.attrib['name'])
+
+        children = complex_type.findall(els_path , ns)
+        if children is not None:
+            temp_config[node.attrib['name']]['children'] = []
+            for child in children:
+                print "=========="
+                print child.attrib['name']
+                print "=========="
+                temp_config[node.attrib['name']]['children'].\
+                    append(get_children(child))
+    return temp_config
+
     #fh.close()
-# for node in fin_config_nodes:
-#     if node != None:
-#         print "Name: ", node.attrib['name']
+
+config = []
+for node in fin_config_nodes:
+    ct_path = "xs:complexType"
+    el_seq_path = "xs:sequence"
+    attr_path = "xs:attribute"
+    if node != None:
+        config.append(get_children(node))
+
+print config
